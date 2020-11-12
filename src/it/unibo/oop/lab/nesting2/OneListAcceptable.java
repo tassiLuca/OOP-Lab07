a776965f27ebce30ea968c15a7144b9c2aec5711
Luca Tassinari <luca.tassinari.2000@gmail.com>
2020-11-12 16:51:17 +0100

    Added blank Acceptable's impl

---
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -0,0 +1,11 @@
+package it.unibo.oop.lab.nesting2;
+
+public class OneListAcceptable<T> implements Acceptable<T> {
+	
+	@Override
+	public Acceptor<T> acceptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}	
+	
+}
