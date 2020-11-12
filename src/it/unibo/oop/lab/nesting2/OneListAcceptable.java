f58e2828a8a511ac7b03900bc5c3ce662e5a1a39
Luca Tassinari <luca.tassinari.2000@gmail.com>
2020-11-12 16:53:25 +0100

    Added constructor impl

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -1,6 +1,15 @@
 package it.unibo.oop.lab.nesting2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OneListAcceptable<T> implements Acceptable<T> {
+	
+	private final List<T> list;
+	
+	public OneListAcceptable(final List<T> list) {
+		this.list = new ArrayList<>(list);
+	}
 	
 	@Override
 	public Acceptor<T> acceptor() {
